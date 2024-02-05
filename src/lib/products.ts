f698c91206320c71916be6540ecf8e227dd5fdff
Fabricio Zillig <fabricio.zillig@gmail.com>
2024-02-05 19:17:47 -0300

    Alteração do Readme

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -77,43 +77,5 @@ export const productsList = [
       stock: 10,
       image: "https://images.unsplash.com/photo-1503153181849-4e4f85a341ce?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       description: "A tulipa é uma planta herbácea, perene, rizomatosa, de folhagem ornamental, muito utilizada em jardins e vasos, devido à sua resistência e baixa necessidade de manutenção. Ela é originária da África tropical e pertence à família das Ruscaceae."
-   },{
-      id: uuidv4(),
-      category: "Exterior",
-      product_name: "Espada de São Jorge",
-      sub_name: "Dracaena trifasciata",
-      price: 59.9,
-      stock: 10,
-      image: "https://images.unsplash.com/photo-1509423350716-97f9360b4e09?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "A espada-de-são-jorge é uma planta herbácea, perene, rizomatosa, de folhagem ornamental, muito utilizada em jardins e vasos, devido à sua resistência e baixa necessidade de manutenção. Ela é originária da África tropical e pertence à família das Ruscaceae."
-   },
-   {
-      id: uuidv4(),
-      category: "Interior",
-      product_name: "Tulipa",
-      sub_name: "Tulipa hybrida Hort",
-      price: 89.5,
-      stock: 10,
-      image: "https://images.unsplash.com/photo-1503153181849-4e4f85a341ce?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "A tulipa é uma planta herbácea, perene, rizomatosa, de folhagem ornamental, muito utilizada em jardins e vasos, devido à sua resistência e baixa necessidade de manutenção. Ela é originária da África tropical e pertence à família das Ruscaceae."
-   },{
-      id: uuidv4(),
-      category: "Exterior",
-      product_name: "Espada de São Jorge",
-      sub_name: "Dracaena trifasciata",
-      price: 59.9,
-      stock: 10,
-      image: "https://images.unsplash.com/photo-1509423350716-97f9360b4e09?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "A espada-de-são-jorge é uma planta herbácea, perene, rizomatosa, de folhagem ornamental, muito utilizada em jardins e vasos, devido à sua resistência e baixa necessidade de manutenção. Ela é originária da África tropical e pertence à família das Ruscaceae."
-   },
-   {
-      id: uuidv4(),
-      category: "Interior",
-      product_name: "Tulipa",
-      sub_name: "Tulipa hybrida Hort",
-      price: 89.5,
-      stock: 10,
-      image: "https://images.unsplash.com/photo-1503153181849-4e4f85a341ce?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      description: "A tulipa é uma planta herbácea, perene, rizomatosa, de folhagem ornamental, muito utilizada em jardins e vasos, devido à sua resistência e baixa necessidade de manutenção. Ela é originária da África tropical e pertence à família das Ruscaceae."
    }
 ];
